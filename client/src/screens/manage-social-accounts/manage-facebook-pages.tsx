bf9ad89ab8417994ff1b2bcb6c832ccbb495314f
Patriksafar <safarpatrik@seznam.cz>
2020-04-07 23:36:36 +0200

    facebook page list scuring

--- client/src/screens/manage-social-accounts/manage-facebook-pages.tsx
+++ client/src/screens/manage-social-accounts/manage-facebook-pages.tsx
@@ -5,21 +5,52 @@ import { Avatar } from "../../components/avatar";
 import { Redirect } from "@reach/router";
 import { routes } from "../../config";
 import { postData, getData } from "../../utils";
+import { Typography } from "@material-ui/core";
 
 export const ManageFacebookPages = () => {
   const { fbUserData, veenupToken } = useStore();
-  const [listOfPages, setListOfPages] = useState([]);
-
-  // need to compare pages on DB with pages from facebook stored in listOfPages
-  getData("http://localhost:5000/social-page/me", veenupToken).then(
-    (response) => {
-      console.log(response);
-      console.log(listOfPages);
-    }
+  const [listOfFacebookPages, setListOfFacebookPages] = useState<[] | null>(
+    null
   );
+  const [listOfConnectedPages, setListOfConnectedPages] = useState<any>(null);
+  const [availablePages, setAvailablePages] = useState<any>(null);
+
+  useEffect(() => {
+    if (!listOfConnectedPages)
+      // need to compare pages on DB with pages from facebook stored in listOfPages
+      getData("http://localhost:5000/social-page/me", veenupToken).then(
+        (response) => {
+          setListOfConnectedPages(response);
+        }
+      );
+  });
+
+  useEffect(() => {
+    const array: any = [];
+    if (!listOfFacebookPages) {
+      return;
+    }
+
+    listOfFacebookPages.map((facebookPage: any, index: number) => {
+      const shouldBeRendered =
+        listOfConnectedPages &&
+        listOfConnectedPages
+          .map((e: Record<string, string>) => {
+            return e.name;
+          })
+          .indexOf(facebookPage.name);
+
+      if (shouldBeRendered === -1) {
+        array.push(facebookPage);
+      } else {
+        return;
+      }
+    });
+
+    setAvailablePages(array);
+  }, [listOfConnectedPages, listOfFacebookPages]);
 
   const handleLinkPageClick = (page: any) => {
-    console.log(page);
     const { access_token, picture, name } = page;
     if (access_token && picture && name) {
       const payload = {
@@ -30,7 +61,14 @@ export const ManageFacebookPages = () => {
       };
       postData("http://localhost:5000/social-page/new", payload, veenupToken)
         .then((response) => {
-          console.log(response);
+          if (response.page && listOfConnectedPages) {
+            const newPage = response.page;
+
+            setListOfConnectedPages((prevState: any) => [
+              ...prevState,
+              newPage,
+            ]);
+          }
         })
         .catch((error) => console.log(error));
     }
@@ -43,24 +81,21 @@ export const ManageFacebookPages = () => {
       .then((response) => response.json())
       .then((responseData) => {
         const { data } = responseData;
-        setListOfPages(data);
+        setListOfFacebookPages(data);
       });
   };
 
   return (
     <div>
       <Button onClick={handleAddPagesButton}>Add Facebook page</Button>
-      {listOfPages && (
+      {listOfConnectedPages && listOfConnectedPages.length > 0 && (
         <Paper>
-          {listOfPages.map((page: any) => (
+          Those are connected:
+          {listOfConnectedPages.map((page: any) => (
             <Box
-              key={page.id}
+              key={page._id}
               startAdornment={
-                <Avatar
-                  variant="rounded"
-                  src={page.picture.data.url}
-                  alt={page.name}
-                />
+                <Avatar variant="rounded" src={page.image} alt={page.name} />
               }
               endAdornment={
                 <Button
@@ -68,13 +103,47 @@ export const ManageFacebookPages = () => {
                     handleLinkPageClick(page);
                   }}
                 >
-                  Link it
+                  Connected
                 </Button>
               }
             >
               {page.name}
             </Box>
           ))}
+        </Paper>
+      )}
+
+      {availablePages && (
+        <Paper>
+          {availablePages.length > 0 ? (
+            availablePages.map((facebookPage: any) => {
+              return (
+                <Box
+                  key={facebookPage.id}
+                  startAdornment={
+                    <Avatar
+                      variant="rounded"
+                      src={facebookPage.picture.data.url}
+                      alt={facebookPage.name}
+                    />
+                  }
+                  endAdornment={
+                    <Button
+                      onClick={() => {
+                        handleLinkPageClick(facebookPage);
+                      }}
+                    >
+                      Link it
+                    </Button>
+                  }
+                >
+                  {facebookPage.name}
+                </Box>
+              );
+            })
+          ) : (
+            <Typography>No more pages to connect</Typography>
+          )}
         </Paper>
       )}
     </div>
